d915721a4eea52dd997d4d57a90786b0df506a9b
Jakub <49204020+Panik1997@users.noreply.github.com>
2020-01-11 19:44:22 +0100

    Update SQL_TASK.sql

--- SQL_TASK.sql
+++ SQL_TASK.sql
@@ -1,10 +1,10 @@
-1.  Select countries where the total number of inhabitants (population) in all cities is greater
-than 400.
+/*1.  Select countries where the total number of inhabitants (population) in all cities is greater
+than 400.*/
 
 SELECT c.Name FROM Country AS c 
 WHERE 400 < (SELECT ci.Population FROM City AS ci WHERE c.id_CountryID = ci.CountryID)
 
-2. Select names of the countries that have no buildings at all
+/*2. Select names of the countries that have no buildings at all*/
 
 SELECT c.Name FROM Country AS c 
 JOIN City AS ci ON c.CountryID = ci.CountryID 
