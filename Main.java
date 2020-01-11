cf8c2903e57994c6708c709cd8ed950ab850c660
Jakub <49204020+Panik1997@users.noreply.github.com>
2020-01-11 19:20:45 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -28,12 +28,14 @@ public class Main {
     public static void addInfo(String str, String[] str1, int n){
         str1[n] = str;
     }
+    
+    //I know few methods above work the same, but I've created them to let the code be cleaner
 
     public static void addToFile(Lunch obj, int n){
         try {
             File file = new File("order.txt");
             FileWriter filewriter = new FileWriter(file, true);
-            filewriter.write(n + ". " + obj.lunch_name + " " + obj.lunch_price + " $\n");
+            filewriter.write(n + ". " + obj.lunch_name + " " + obj.lunch_price + " PLN\n");
             filewriter.close();
         }
             catch (IOException e){
@@ -45,7 +47,7 @@ public class Main {
         try {
             File file = new File("order.txt");
             FileWriter filewriter = new FileWriter(file, true);
-            filewriter.write(n + ". " + obj.name_of_drink + " " + obj.price_of_drink + " $\n");
+            filewriter.write(n + ". " + obj.name_of_drink + " " + obj.price_of_drink + " PLN\n");
             filewriter.close();
         }
         catch (IOException e){
@@ -57,7 +59,7 @@ public class Main {
         try {
             File file = new File("order.txt");
             FileWriter filewriter = new FileWriter(file, true);
-            filewriter.write("------------------------------------\n" + "Sum: " + k + " $" + "\n------------------------------------");
+            filewriter.write("------------------------------------\n" + "Sum: " + k + " PLN" + "\n------------------------------------");
             filewriter.close();
         }
         catch (IOException e){
@@ -100,6 +102,9 @@ public class Main {
         Scanner num = new Scanner(System.in);
         double bill1 = 0;
 
+        // If you want add new meal to menu, just create another object 
+        // of type Lunch in array "meals" and add +1 to index.
+        
         Lunch[] meals = new Lunch[9];
         Lunch[] italianFoodArray = new Lunch[100];
         Lunch[] polishFoodArray = new Lunch[100];
@@ -140,7 +145,7 @@ public class Main {
                         System.out.println("Which meal do you want to buy?\n");
                         for(int i = 0 ; i < meals.length ; i++){
                             if(meals[i].getCountry_name().equals("Italian") ){
-                                System.out.println(n+1 + ". " + meals[i].lunch_name + ", Price: " + meals[i].lunch_price + " $");
+                                System.out.println(n+1 + ". " + meals[i].lunch_name + ", Price: " + meals[i].lunch_price + " PLN");
                                 addItalianMeal(meals[i], italianFoodArray, n);
                                 n++;
                             }
@@ -151,7 +156,7 @@ public class Main {
                             System.out.println("Wrong meal!");
                             break;
                         } else {
-                            System.out.println("You have chosen: \n" + italianFoodArray[number1 - 1].lunch_name + ", Price: " + italianFoodArray[number1 - 1].lunch_price + " $");
+                            System.out.println("You have chosen: \n" + italianFoodArray[number1 - 1].lunch_name + ", Price: " + italianFoodArray[number1 - 1].lunch_price + " PLN");
                             addToOrder(italianFoodArray[number1 - 1], orderArray, j);
                             j++;
                             System.out.println("-------------------------------------------------------------------------");
@@ -162,14 +167,14 @@ public class Main {
                         if(number2 == 1){
                             System.out.println("Choose drink: ");
                             for(int i = 0; i < drinks.length ; i++){
-                                System.out.println(i+1 + ". " + drinks[i].name_of_drink + ", Price: " + drinks[i].price_of_drink + " $");
+                                System.out.println(i+1 + ". " + drinks[i].name_of_drink + ", Price: " + drinks[i].price_of_drink + " PLN");
                             }
                             int number3 = num.nextInt();
                             if(number3 > drinks.length || number3 <= 0){
                                 System.out.println("Wrong drink!!!");
                                 break;
                             } else {
-                                System.out.println("You have chosen: \n" + drinks[number3 - 1].name_of_drink + ", Price: " + drinks[number3 - 1].price_of_drink + " $\n");
+                                System.out.println("You have chosen: \n" + drinks[number3 - 1].name_of_drink + ", Price: " + drinks[number3 - 1].price_of_drink + " PLN\n");
                                 addDrinkToOrder(drinks[number3-1], drinksOrder, m);
                                 m++;
 
@@ -199,7 +204,7 @@ public class Main {
                         n=0;
                         for(int i = 0 ; i < meals.length ; i++){
                             if(meals[i].getCountry_name().equals("Polish") ){
-                                System.out.println(n+1 + ". " + meals[i].lunch_name + " Price: " + meals[i].lunch_price + " $");
+                                System.out.println(n+1 + ". " + meals[i].lunch_name + " Price: " + meals[i].lunch_price + " PLN");
                                 addPolishMeal(meals[i], polishFoodArray, n);
                                 n++;
                             }
@@ -210,7 +215,7 @@ public class Main {
                             System.out.println("Wrong meal!");
                             break;
                         } else {
-                            System.out.println("You have chosen: \n" + polishFoodArray[number1 - 1].lunch_name + ", Price: " + polishFoodArray[number1 - 1].lunch_price);
+                            System.out.println("You have chosen: \n" + polishFoodArray[number1 - 1].lunch_name + ", Price: " + polishFoodArray[number1 - 1].lunch_price + " PLN");
                             addToOrder(polishFoodArray[number1 - 1], orderArray, j);
                             j++;
                             System.out.println("-------------------------------------------------------------------------");
@@ -221,11 +226,11 @@ public class Main {
                         if(number4 == 1){
                             System.out.println("Choose drink: ");
                             for(int i = 0; i < drinks.length ; i++){
-                                System.out.println(i+1 + ". " + drinks[i].name_of_drink + ", Price: " + drinks[i].price_of_drink + " $");
+                                System.out.println(i+1 + ". " + drinks[i].name_of_drink + ", Price: " + drinks[i].price_of_drink + " PLN");
                             }
                             int number3 = num.nextInt();
                             if(number3 <= drinks.length && number3 > 0){
-                                System.out.println("You have chosen: \n" + drinks[number3 - 1].name_of_drink + ", Price: " + drinks[number3 - 1].price_of_drink + " $\n");
+                                System.out.println("You have chosen: \n" + drinks[number3 - 1].name_of_drink + ", Price: " + drinks[number3 - 1].price_of_drink + " PLN\n");
                                 addDrinkToOrder(drinks[number3 - 1], drinksOrder, m);
                                 m++;
 
@@ -256,7 +261,7 @@ public class Main {
                         n=0;
                         for(int i = 0 ; i < meals.length ; i++){
                             if(meals[i].getCountry_name().equals("Mexican") ){
-                                System.out.println(n+1 + ". " + meals[i].lunch_name + " Price: " + meals[i].lunch_price + " $");
+                                System.out.println(n+1 + ". " + meals[i].lunch_name + " Price: " + meals[i].lunch_price + " PLN");
                                 addMexicanMeal(meals[i], mexicanFoodArray, n);
                                 n++;
                             }
@@ -278,11 +283,11 @@ public class Main {
                         if(number5 == 1){
                             System.out.println("Choose drink: ");
                             for(int i = 0; i < drinks.length ; i++){
-                                System.out.println(i+1 + ". " + drinks[i].name_of_drink + ", Price: " + drinks[i].price_of_drink + " $");
+                                System.out.println(i+1 + ". " + drinks[i].name_of_drink + ", Price: " + drinks[i].price_of_drink + " PLN");
                             }
                             int number6 = num.nextInt();
                             if(number6 <= drinks.length && number6 > 0){
-                                System.out.println("You have chosen: \n" + drinks[number6 - 1].name_of_drink + ", Price: " + drinks[number6 - 1].price_of_drink + " $\n");
+                                System.out.println("You have chosen: \n" + drinks[number6 - 1].name_of_drink + ", Price: " + drinks[number6 - 1].price_of_drink + " PLN\n");
                                 addDrinkToOrder(drinks[number6 - 1], drinksOrder, m);
                                 m++;
 
@@ -316,18 +321,18 @@ public class Main {
                             break;
                         } else {
                             for (int i = 0; i < j; i++) {
-                                System.out.println(tmp + ". " + orderArray[i].lunch_name + " " + orderArray[i].lunch_price + " $");
+                                System.out.println(tmp + ". " + orderArray[i].lunch_name + " " + orderArray[i].lunch_price + " PLN");
                                 bill = bill + orderArray[i].lunch_price;
                                 tmp++;
                             }
                             for (int i = 0; i < m ; i++) {
-                                System.out.println(tmp + ". " + drinksOrder[i].name_of_drink + " " + drinksOrder[i].price_of_drink + " $");
+                                System.out.println(tmp + ". " + drinksOrder[i].name_of_drink + " " + drinksOrder[i].price_of_drink + " PLN");
                                 bill = bill + drinksOrder[i].price_of_drink;
                                 tmp++;
                             }
 
                             System.out.println("-------------------------------------------------------------------------");
-                            System.out.println("To pay: " + bill + " $");
+                            System.out.println("To pay: " + bill + " PLN");
                             System.out.println("-------------------------------------------------------------------------\n");
 
                             System.out.println("\nAdditional informations:\n");
